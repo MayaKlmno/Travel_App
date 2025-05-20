288d6c93a8d0c7485e9a8dfd05ad5b7bb8209e80
Maya Oniciuc <maya.klmno@gmail.com>
2025-05-20 08:48:43 -0700

    added more missing files

---
+++ app/src/main/java/com/example/travelapp/Constrants.kt
@@ -0,0 +1,10 @@
+package com.example.travelapp
+
+object Constrants {
+    // to make sure that this part isn't posted on github make this specific file unseen on git
+    // add this to Git ignore
+    // why can't i find this on github??
+    // TODO: Fill these in
+    val API_KEY = ""
+    val APP_ID = ""
+}
